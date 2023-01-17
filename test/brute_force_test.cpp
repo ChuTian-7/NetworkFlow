916b5a2cab93df27ed61e1df222fd3f5f53f8fc7
ChuTian-7 <yuhan@icbench.com>
2023-01-17 16:48:56 +0800

    more tests

--- test/brute_force_test.cpp
+++ test/brute_force_test.cpp
@@ -9,7 +9,49 @@ std::deque<Graph> G={
     {0,0,[](int x){return 0;}},
     {-2,2,[](int x){return 2*x*x-3*x;}},
   },
-  {})
+  {}),
+  Graph(2,1,
+	{
+		{0,  0, [](int) { return 0; }},
+		{-2, 2, [](int x) { return -x; }},
+		{-2, 2, [](int x) { return +x; }},
+	},
+	{
+		{-4, 4, [](int x) { return (x - 3) * (x - 3); }, 1, 2}
+	}),
+  Graph(
+    2,1,
+		{
+			{0,  0, [](int) { return 0; }},
+			{-2, 2, [](int x) { return -x; }},
+			{-2, 2, [](int x) { return +x; }},
+		},
+		{
+			{-4, 4, [](int x) { return (x + 3) * (x + 3); }, 1, 2}
+		}
+  ),
+  Graph(
+    2,1,
+		{
+			{0,  0, [](int) { return 0; }},
+			{-4, 4, [](int x) { return x*x; }},
+			{-5, 3, [](int x) { return 3*x*x+4*x; }},
+		},
+		{
+			{-3, 3, [](int x) { return 2*x*x-3*x; }, 1, 2}
+		}
+  ),
+  Graph(
+    2,1,
+		{
+			{0,  0, [](int) { return 0; }},
+			{-4, 4, [](int x) { return x*x; }},
+			{-5, 3, [](int x) { return 3*x*x+4*x; }},
+		},
+		{
+			{-10, -7, [](int x) { return 2*x*x-3*x; }, 1, 2}
+		}
+  )
 };
 
 std::deque<int> ans;
